50f3abd7c9bca5dc1bc115fc343193b03de82e30
Anna <anna.elisabeth@gmail.com>
2024-01-30 12:19:42 +0000

    first commit

---
+++ src/main/java/com/lbg/cana/CanaApplication.java
@@ -0,0 +1,13 @@
+package com.lbg.cana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lbg/cana/CanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbg.cana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
