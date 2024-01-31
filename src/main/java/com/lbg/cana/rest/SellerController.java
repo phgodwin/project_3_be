22e47eed4c799b72b6716f359104cfbda6c47965
User <agustinadipaolauk@gmail.com>
2024-01-31 13:13:20 +0000

    update sellers

--- src/main/java/com/lbg/cana/rest/SellerController.java
+++ src/main/java/com/lbg/cana/rest/SellerController.java
@@ -1,12 +1,20 @@
 package com.lbg.cana.rest;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lbg.cana.domain.Seller;
 import com.lbg.cana.service.SellerService;
 
 @RestController
-@RequestMapping("/seller")
+@RequestMapping("/sellers")
 
 public class SellerController {
 
@@ -18,4 +26,21 @@ public class SellerController {
 
 	}
 
+	@PostMapping("/create")
+
+	public ResponseEntity<Seller> createSeller(@RequestBody Seller newSeller) {
+		return this.service.createSeller(newSeller);
+	}
+
+	@GetMapping("/get")
+
+	public List<Seller> getSeller() {
+		return this.service.getSeller();
+
+	}
+
+	@GetMapping("/get/{id}")
+	public ResponseEntity<Seller> getSeller(@PathVariable int id) {
+		return this.service.getSeller(id);
+	}
 }
--- src/main/java/com/lbg/cana/service/SellerService.java
+++ src/main/java/com/lbg/cana/service/SellerService.java
@@ -1,7 +1,13 @@
 package com.lbg.cana.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.lbg.cana.domain.Seller;
 import com.lbg.cana.repo.SellerRepo;
 
 @Service
@@ -13,6 +19,25 @@ public class SellerService {
 	public SellerService(SellerRepo repo) {
 		super();
 		this.repo = repo;
+	}
+
+	public ResponseEntity<Seller> createSeller(Seller newSeller) {
+		Seller created = this.repo.save(newSeller);
+		return new ResponseEntity<Seller>(created, HttpStatus.CREATED);
+	}
+
+	public List<Seller> getSeller() {
+		return this.repo.findAll();
+	}
+
+	public ResponseEntity<Seller> getSeller(int id) {
+		Optional<Seller> found = this.repo.findById(id);
+		if (found.isEmpty()) {
+			return new ResponseEntity<Seller>(HttpStatus.NOT_FOUND);
+		}
+
+		Seller body = found.get();
+		return ResponseEntity.ok(body);
 	}
 
 }
